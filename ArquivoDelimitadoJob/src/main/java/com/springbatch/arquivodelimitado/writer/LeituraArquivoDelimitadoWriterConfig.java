563169576194d668f2783f3d9669cece679a24b0
GFT\fnpa <franklinpr2010@gmail.com>
2021-08-13 10:51:31 -0300

    subindo exemplo de criação  de arquivo delimitado

--- ArquivoDelimitadoJob/src/main/java/com/springbatch/arquivodelimitado/writer/LeituraArquivoDelimitadoWriterConfig.java
+++ ArquivoDelimitadoJob/src/main/java/com/springbatch/arquivodelimitado/writer/LeituraArquivoDelimitadoWriterConfig.java
@@ -1,15 +1,29 @@
 package com.springbatch.arquivodelimitado.writer;
 
+
+
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 import com.springbatch.arquivodelimitado.dominio.Cliente;
 
 @Configuration
 public class LeituraArquivoDelimitadoWriterConfig {
 	@Bean
-	public ItemWriter<Cliente> leituraArquivoDelimitadoWriter() {
-		return items -> items.forEach(System.out::println);
+	public FlatFileItemWriter<Cliente> leituraArquivoDelimitadoWriter(
+			@Value("#{jobParameters[arquivoClientesSaida]}") Resource arquivoClientesSaida) {
+		return new FlatFileItemWriterBuilder<Cliente>()
+				.name("leituraArquivoDelimitadoWriter")
+				.resource(arquivoClientesSaida)
+				.delimited()
+				//Separado por virgula
+				.delimiter(",")
+				.names("nome", "sobrenome", "idade", "email").build();
+		
 	}
 }
--- ArquivoLarguraFixaJob/src/main/java/com/springbatch/arquivolargurafixa/writer/LeituraArquivoLarguraFixaWriterConfig.java
+++ ArquivoLarguraFixaJob/src/main/java/com/springbatch/arquivolargurafixa/writer/LeituraArquivoLarguraFixaWriterConfig.java
@@ -1,15 +1,34 @@
 package com.springbatch.arquivolargurafixa.writer;
 
-import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 import com.springbatch.arquivolargurafixa.dominio.Cliente;
+/**
+ * Escrita e arquivo flat
+ * @author fnpa
+ *
+ */
 
 @Configuration
 public class LeituraArquivoLarguraFixaWriterConfig {
+
+	@StepScope //como está obtendo dados do jobs parameters, tem que colocar o @stepscope
 	@Bean
-	public ItemWriter<Cliente> leituraArquivoLarguraFixaWriter() {
-		return items -> items.forEach(System.out::println);
+	public FlatFileItemWriter<Cliente> escritaArquivoLarguraFixaWriter(@Value("#{jobParameters['arquivoClienteSaida']}") Resource arquivoClienteSaida) {
+		return new FlatFileItemWriterBuilder<Cliente>()
+				.name("escritaArquivoLarguraFixaWriter")
+				.resource(arquivoClienteSaida)
+				//na saida tem que ser format, ou seja criar o arquivo de saida
+				.formatted()
+				.format("%-9s %-9s %-2s %-19")
+				.names(new String[] {"nome", "sobrenome", "idade", "email"})
+				.build();
+		
 	}
 }
